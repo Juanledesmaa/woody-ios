e4bb660d0235a544e743aeec497b3b5eeca7e871
Juan Ledesma <jlmanuel8540@gmail.com>
2022-01-24 01:13:13 -0300

    Add main colors palette struct

---
+++ Woody/Resources/Colors/Colors.swift
@@ -0,0 +1,23 @@
+//
+//  Colors.swift
+//  Woody
+//
+//  Created by juan ledesma on 24/01/2022.
+//
+
+import UIKit
+
+struct Colors {
+    enum MainPalette {
+        /// Powder blue
+        static let backgroundBlue = UIColor(red: 192 / 255, green: 224 / 255, blue: 222 / 255, alpha: 1.0)
+        /// Dark Jungle Green
+        static let buttonBlack = UIColor(red: 22 / 255, green: 37 / 255, blue: 33 / 255, alpha: 1.0)
+        /// Steel Blue
+        static let buttonBlue = UIColor(red: 79 / 255, green: 124 / 255, blue: 172 / 255, alpha: 1.0)
+        /// Charcoal
+        static let rowGray = UIColor(red: 60 / 255, green: 71 / 255, blue: 75 / 255, alpha: 1.0)
+        /// Middle Blue Green
+        static let secondaryButtonBlue = UIColor(red: 158 / 255, green: 239 / 255, blue: 229 / 255, alpha: 1.0)
+    }
+}
